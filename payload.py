780cf04c24703d3e6f377d992a189a1446e85ac4
Rakesh Yadav <ryadav23@hotmail.com>
2021-09-16 14:37:41 -0400

    updated variables

--- payload.py
+++ payload.py
@@ -9,19 +9,19 @@ def index():
 
 @app.route('/manage_file', methods=['GET', 'POST'])
 def manage_file():
+    url = 'https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt'
+    filename = 'sample-text-file.txt'
     if (request.method == 'POST'):
         json_payload = request.get_json()
         if (json_payload['action'] == "download"):
-            url = 'https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt'
             req = requests.get(url)
-            filename = 'sample-text-file.txt'
             with open(filename, 'wb') as f:
                 for chunk in req.iter_content(chunk_size=8192):
                     if chunk:
                         f.write(chunk)
             return filename, 201
         elif (json_payload['action'] == "read"):
-            with open("sample-text-file.txt","r") as file:
+            with open(filename,"r") as file:
                 content = file.read()
             return jsonify(content), 200
         else: 
